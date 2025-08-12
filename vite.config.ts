9a18d00c7a0f53aca2390d5c3d4ce2679d449b63
Meranda Freiner <merandafreiner3@gmail.com>
2025-08-12 16:42:40 -0700

    before change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/kennedy-equity-showcase/",
   server: {
     host: "::",
     port: 8080,
