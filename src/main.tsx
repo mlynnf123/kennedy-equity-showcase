99230383dbc5f02e047be8e606c47378c5ccd3ad
Meranda Freiner <merandafreiner3@gmail.com>
2025-08-12 18:12:18 -0700

    Add debug logging to diagnose deployment issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+console.log('Main.tsx loaded');
+
+const rootElement = document.getElementById("root");
+console.log('Root element:', rootElement);
+
+if (rootElement) {
+  try {
+    const root = createRoot(rootElement);
+    console.log('Root created');
+    root.render(<App />);
+    console.log('App rendered');
+  } catch (error) {
+    console.error('Error rendering app:', error);
+  }
+} else {
+  console.error('Root element not found!');
+}
